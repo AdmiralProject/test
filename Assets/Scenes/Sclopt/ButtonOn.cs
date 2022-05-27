78337c020dd0ed91cbd104ff3fc59fb8fff6b2ae
DESKTOP-SCIFN6J\hide_ <tkproject.sa@gmail.com>
2022-05-27 23:06:45 +0900

    テストプロジェクトです

---
+++ Assets/Scenes/Sclopt/ButtonOn.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOn : MonoBehaviour
+{
+	public void ButtonPushed()
+	{
+		Debug.Log("ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚Ü‚µ‚½");
+	}
+}
+
+
+
